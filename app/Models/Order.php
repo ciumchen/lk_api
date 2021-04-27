8e2e14e51b905d7cd08c17fddab7664cd9f62b51
ciumchen <ciumchen@163.com>
2021-04-27 20:18:03 +0800

    api调试

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -186,6 +186,7 @@ class Order extends Model
             {
                 //通过，给用户加积分、更新LK
                 $customer = User::lockForUpdate()->find($order->uid);
+                Log::info('666666666666666', get_object_vars($customer));
                 //按比例计算实际获得积分
                 $customerIntegral = bcmul($order->price, bcdiv($rebateScale[(int)$order->profit_ratio],100, 4), 2);
                 $amountBeforeChange =  $customer->integral;
