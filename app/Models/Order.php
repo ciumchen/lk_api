71c2afac54eb3594d38ace48b36ec2a9bf387e4c
ciumchen <ciumchen@163.com>
2021-04-27 15:37:56 +0800

    api调试

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -174,8 +174,8 @@ class Order extends Model
     {
         DB::beginTransaction();
         try{
-            $order = Order::lockForUpdate()->where('uid', $uid)->get();
-            Log::info('+++++++', $order);
+            $order = Order::lockForUpdate()->where('uid', $uid)->first();
+            Log::info('+++++++', get_object_vars($order));
             $order->status = $status;
             //用户应返还几分比例
             $userRebateScale = Setting::getManySetting('user_rebate_scale');
