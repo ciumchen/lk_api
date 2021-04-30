6f461d4f10438d7cc9af43bb9311cf7ea9be08ec
ciumchen <ciumchen@163.com>
2021-04-30 17:23:02 +0800

    api调试

--- routes/api/pay.php
+++ routes/api/pay.php
@@ -1,10 +1,13 @@
 <?php
 
-//调用支付接口
-Route::post('payment', 'Payment\AdaPayController@CreatePay');
+Route::middleware(['auth:sanctum'])->group(function () {
+    //调用支付接口
+    Route::post('payment', 'Payment\AdaPayController@CreatePay');
 
-//支付回调地址
-Route::any('notify', 'Payment\NotifyController@callBack');
+    //支付回调地址
+    Route::any('notify', 'Payment\NotifyController@callBack');
 
-//支付失败再次支付
-Route::post('again-pay', 'Payment\AdaPayController@againPay');
+    //支付失败再次支付
+    Route::post('again-pay', 'Payment\AdaPayController@againPay');
+
+});
