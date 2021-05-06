e472f0f65854f78573f188cbb99b98f711d3473f
ning <ning@qq.com>
2021-05-06 15:02:43 +0800

    更新配置

---
+++ app/Http/Controllers/API/Business/GetBusinessController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Http\Controllers\API\Business;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+
+class GetBusinessController extends Controller
+{
+
+    //获取星级商户列表
+    public function getStarBusinessList(){
+
+        $re = Redis::set('key1','1231231');
+
+        var_dump($re);
+        var_dump(Redis::get('key1'));
+//
+//        $user = Redis::get('user:profile:asdfasdfasdf');
+//
+//        var_dump($user);
+
+    }
+
+
+
+
+}
