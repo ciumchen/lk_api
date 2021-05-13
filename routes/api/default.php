dc1e4b2a40d60cc2af590b68d19437fc98b94b0b
ning <ning@qq.com>
2021-05-13 16:21:53 +0800

    修改获取星级商家接口和路由

--- routes/api/default.php
+++ routes/api/default.php
@@ -12,8 +12,7 @@ Route::get("get-business-category","Business\BusinessController@getBusinessCateg
 //获取广告
 Route::get("get-ad","Business\BusinessController@getAd");
 //获取商家列表-旧
-//Route::any("get-business-list","Business\BusinessController@getBusinessList");
-
+Route::any("get-business-list","Business\BusinessController@getBusinessList");
 //获取商家详情
 Route::get("get-business-info","Business\BusinessController@getBusinessInfo");
 
@@ -21,11 +20,10 @@ Route::get("get-business-info","Business\BusinessController@getBusinessInfo");
 //获取星级商户列表-新
 Route::get("getStarBusinessList","Business\GetBusinessController@getStarBusinessList");
 
-//获取星级商户列表-商家页-分类筛选搜索
+//获取所有商户列表-商家页-分类筛选搜索
 Route::get("getAllBusinessList","Business\GetBusinessController@getAllBusinessList");
 
-//获取商家列表-旧
-Route::any("get-business-list","Business\BusinessController@getStarBusinessList");
+
 
 
 
