2d16dd20d0258c3b06d63c0050aede7aaff452fa
lidong <947714443@qq.com>
2021-05-17 17:49:03 +0800

    fixed notify callbacks route

--- app/Models/TradeOrder.php
+++ app/Models/TradeOrder.php
@@ -62,7 +62,6 @@ class TradeOrder extends Model
             $order = $val;
         }
         $orderData = get_object_vars($order);
-        dd($orderData);
         if ( !$orderInfo)
             throw new LogicException('订单不存在');
         if ($orderData[ 'status' ] != 'await')
