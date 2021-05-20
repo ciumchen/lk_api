20b0ab15e72a94f6c4bcb6750c6a3723213725a2
lidong <947714443@qq.com>
2021-05-20 20:27:33 +0800

    add cache_clear.sh

---
+++ cache_clear.sh
@@ -0,0 +1,16 @@
+#!/usr/bin
+
+#清理配置缓存
+php artisan config:clear
+
+#生成配置缓存
+# php artisan config:cache
+
+#清理配置缓存
+php artisan route:clear
+
+#生成配置缓存
+# php artisan route:cache
+
+#清理缓存
+php artisan cache:clear
