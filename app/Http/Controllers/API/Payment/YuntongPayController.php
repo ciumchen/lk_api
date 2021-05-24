c80b230e8cc0d5a727b8e2e279f98cacbe7a3965
lidong <947714443@qq.com>
2021-05-24 14:14:16 +0800

    https ceshi

--- app/Http/Controllers/API/Payment/YuntongPayController.php
+++ app/Http/Controllers/API/Payment/YuntongPayController.php
@@ -105,7 +105,6 @@ class YuntongPayController extends Controller
             if (isset($data[ 'return_url' ])) {
                 $res = $res->setReturnUrl($data[ 'return_url' ]);
             }
-            return response()->json([$return_url]);
             $res = $res->pay();
             $response = json_decode($res, true);
             return response()->json(['url' => $response[ 'pay_url' ]]);
