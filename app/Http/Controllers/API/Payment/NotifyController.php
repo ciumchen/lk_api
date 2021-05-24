0ec351ce98863c89fd3115e40c4c760c26b2b885
ciumchen <ciumchen@163.com>
2021-05-24 14:50:51 +0800

    佐兰话费充值

--- app/Http/Controllers/API/Payment/NotifyController.php
+++ app/Http/Controllers/API/Payment/NotifyController.php
@@ -101,7 +101,8 @@ class NotifyController extends Controller
                 //自动充值
                 if ($json_data['description'] == "HF")
                 {
-                    (new RechargeController())->setCall($callData);
+                    //(new RechargeController())->setCall($callData);
+                    (new RechargeController())->callDefray($callData);
                 } elseif ($json_data['description'] == "YK")
                 {
                     (new RechargeController())->setGas($gasData);
