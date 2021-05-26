d34deff89a652db34142a1c5f2ffbb5d4761b480
ciumchen <ciumchen@163.com>
2021-05-26 14:24:23 +0800

    消息系统

--- routes/api/message.php
+++ routes/api/message.php
@@ -1,6 +1,6 @@
 <?php
 
-/*Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     //获取用户消息
     Route::get('get-msg', 'Message\UserMsgController@getMsg');
 
@@ -25,22 +25,4 @@
     //删除所有消息
     Route::get('del-all-msg', 'Message\UserMsgController@delAllMsg');
 
-});*/
-
-//获取系统消息
-Route::get('get-msg', 'Message\UserMsgController@getMsg');
-
-//获取系统消息
-Route::get('get-sys-msg', 'Message\UserMsgController@getSysMsg');
-
-//获取系统消息小红点
-Route::any('get-sys-reddot', 'Message\UserMsgController@getSysReddot');
-
-//删除系统消息小红点
-Route::any('del-sys-reddot', 'Message\UserMsgController@delSysReddot');
-
-//删除消息
-Route::get('del-msg', 'Message\UserMsgController@delMsg');
-
-//删除所有消息
-Route::get('del-all-msg', 'Message\UserMsgController@delAllMsg');
+});
