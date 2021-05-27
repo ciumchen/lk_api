9c31ac1326eb4bb611be5c3c7e4ac99f71f7207f
lidong <947714443@qq.com>
2021-05-27 16:55:15 +0800

    password change test

--- app/Http/Controllers/API/User/UserController.php
+++ app/Http/Controllers/API/User/UserController.php
@@ -333,14 +333,15 @@ class UserController extends Controller
         $password = $request->input('password');
         $new_password = $request->input('new_password');
         $confirm_password = $request->input('confirm_password');
-        return response()->json(['password'         => $password,
-                                 'new_password'     => $new_password,
-                                 'confirm_password' => $confirm_password,
-        ]);
+//        return response()->json(['password'         => $password,
+//                                 'new_password'     => $new_password,
+//                                 'confirm_password' => $confirm_password,
+//        ]);
         if ($new_password != $confirm_password) {
             throw new LogicException('两次输入密码不一致');
         }
         $user = $request->user();
+        return response()->json($user);
         if ($user->verifyPassword($password) == false) {
             throw new LogicException('原密码错误');
         }
