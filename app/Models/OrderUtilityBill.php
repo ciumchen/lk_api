05d9414dbfa3a3161b1aef3c03316d69acaa6133
laydown <947714443@qq.com>
2021-06-02 16:27:56 +0800

    bmapi add folders

---
+++ app/Models/OrderUtilityBill.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * TODO:水电煤订单数据操作
+ */
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderUtilityBill extends Model
+{
+    
+    use HasFactory;
+}
