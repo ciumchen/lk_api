c8670d359fe47232bc0d6f065bf5e085d39bc62c
laydown <947714443@qq.com>
2021-06-05 21:15:00 +0800

    mobile recharge test 10

--- app/Http/Controllers/API/Order/MobileNotifyController.php
+++ app/Http/Controllers/API/Order/MobileNotifyController.php
@@ -14,5 +14,15 @@ class MobileNotifyController extends Controller
     {
         $data = $request->all();
         Log::debug('MobileNotify', [json_encode($data)]);
+        /*
+        {
+        "user_id": "A5626842",
+        "sign": "C0F9E3501C0DB8EBA781993D8268B073FBF9EE79",
+        "recharge_state": "1",
+        "outer_tid": "PY_20210605210408281427",
+        "tid": "S2106052397812",
+        "timestamp": "2021-06-05 21:05:12"
+        }
+        */
     }
 }
