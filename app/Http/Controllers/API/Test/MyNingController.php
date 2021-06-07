244f9426c833568088403883543fb08834dab595
ning <ning@qq.com>
2021-06-07 16:08:58 +0800

    开启自动审核

--- app/Http/Controllers/API/Test/MyNingController.php
+++ app/Http/Controllers/API/Test/MyNingController.php
@@ -77,12 +77,12 @@ class MyNingController extends Controller
     public function pushOrder(){
         set_time_limit(0);
         ini_set('max_execution_time', '0');
-//        $count = Order::where('status',"!=",2)->where('id','>',13281)->where('pay_status',"!=","ddyc")->count();
-        $count = Order::where('status',"!=",2)->where('pay_status',"!=","ddyc")->count();
+        $count = Order::where('status',"!=",2)->where('id','>',23314)->where('pay_status',"!=","ddyc")->count();
+//        $count = Order::where('status',"!=",2)->where('pay_status',"!=","ddyc")->count();
 
         if ($count){
-//            $orderInfo = DB::table('order')->where('order.id','>','13281')
-            $orderInfo = DB::table('order')
+            $orderInfo = DB::table('order')->where('order.id','>','23314')
+//            $orderInfo = DB::table('order')
                 ->where('order.status',"!=",2)
                 ->where('order.pay_status',"!=","ddyc")
                 ->leftJoin('trade_order','order.id','=','trade_order.oid')
