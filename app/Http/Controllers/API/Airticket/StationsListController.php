f8f344e7fc28b5a57f75ec2106bdf532be66f64e
xt chen <ciumchen@163.com>
2021-06-09 14:00:56 +0800

    机票信息

--- app/Http/Controllers/API/Airticket/StationsListController.php
+++ app/Http/Controllers/API/Airticket/StationsListController.php
@@ -27,7 +27,7 @@ class StationsListController extends Controller
         {
             foreach ($value as $val)
             {
-                var_dump($val['item']);
+                var_dump(array_values($val['item']));
                 $items[] = $val['item'];
             }
         }
