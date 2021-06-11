d3c5b6c4d1abbf6d6716c72072a6c03a97bf79e1
xt chen <ciumchen@163.com>
2021-06-11 10:45:44 +0800

    [UPD]商家积分优化

--- routes/api/shop.php
+++ routes/api/shop.php
@@ -2,16 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     //获取积分记录
     Route::get('log-list', 'Shop\GetIntegralController@logsList');
 
     //获取排队积分记录
     Route::get('line-list', 'Shop\GetIntegralController@lineList');
-});*/
-
-//获取积分记录
-Route::get('log-list', 'Shop\GetIntegralController@logsList');
-
-//获取排队积分记录
-Route::get('line-list', 'Shop\GetIntegralController@lineList');
+});
