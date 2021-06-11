5be0dadf9d48f36c139c02cf080d0db512dbafab
xt chen <ciumchen@163.com>
2021-06-11 15:36:31 +0800

    [UPD]机票代码优化

--- app/Http/Controllers/API/Airticket/LinesListController.php
+++ app/Http/Controllers/API/Airticket/LinesListController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API\Airticket;
 
+use App\Exceptions\LogicException;
 use App\Http\Controllers\Controller;
 use Bmapi\Api\Air\LinesList;
 use Illuminate\Http\Request;
@@ -17,6 +18,12 @@ class LinesListController extends Controller
      */
     public function linesList(Request $request)
     {
+        //检查日期数据
+        if (strtotime($request->date) < time() - 86400)
+        {
+            throw new LogicException('日期必须大于当前日期');
+        }
+
         //获取数据
         $linesList = new LinesList();
         $linesInfo = $linesList->setFrom($request->from)
