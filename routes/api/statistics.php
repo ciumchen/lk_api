120818d0e9e76af30a3756cbfef3ba90fc35e1f2
ning <ning@qq.com>
2021-06-11 16:52:08 +0800

    添加今日和昨日添加排队积分消费金额显示接口

--- routes/api/statistics.php
+++ routes/api/statistics.php
@@ -11,4 +11,4 @@ Route::get('getNewStatistics', 'Statistics\StatisticsController@getNewStatistics
 Route::any('shRlCount', 'Statistics\StatisticsController@shRlCount');
 
 //获取今日排队和昨日排队订单的消费金额的让利比例的统计（5%-10%-20%）
-Route::get('getGiveOderPrice', 'Statistics\StatisticsController@getGiveOderPrice');
+Route::any('getGiveOderPrice', 'Statistics\StatisticsController@getGiveOderPrice');
