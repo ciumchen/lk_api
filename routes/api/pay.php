81cf31286bac6837dc0c83d2b232d63a6b982991
xt chen <ciumchen@163.com>
2021-06-17 10:28:53 +0800

    [UPD]机票订单优化

--- routes/api/pay.php
+++ routes/api/pay.php
@@ -18,7 +18,7 @@ Route::post('yun-notify', 'Payment\YuntongNotifyController@callBack');
 //机票支付
 Route::post('air-pay', 'Payment\YuntongPayController@airPay');
 //机票回调支付
-Route::post('air-notify', 'Payment\YuntongPayController@airPayNotify');
+Route::post('air-notify', 'Payment\YuntongNotifyController@airPayNotify');
 //机票二次支付
 Route::post('air-again-pay', 'Payment\YuntongPayController@airAgainPay');
 /****** *******/
