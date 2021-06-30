5ece8592a4215125c886697e35da93d21d54dc56
laydown <947714443@qq.com>
2021-06-30 18:43:08 +0800

    add hotel service

--- app/Http/Controllers/API/Order/HotelController.php
+++ app/Http/Controllers/API/Order/HotelController.php
@@ -2,10 +2,61 @@
 
 namespace App\Http\Controllers\API\Order;
 
+use App\Exceptions\LogicException;
 use App\Http\Controllers\Controller;
+use App\Services\ShowApi\OrderHotelService;
+use Exception;
 use Illuminate\Http\Request;
+use Wanwei\Api\RequestBase;
 
 class HotelController extends Controller
 {
-    //
+    protected $service;
+    
+    public function __construct()
+    {
+        $this->service = new OrderHotelService();
+    }
+    
+    public function search(Request $request)
+    {
+        $page = $request->input('page');
+        $limit = $request->input('limit');
+        $cityName = $request->input('city_name');
+        $inDate = $request->input('in_date');
+        $outDate = $request->input('out_date');
+        $sortKey = $request->input('sort_key');
+        $star = $request->input('star');
+        $minPrice = $request->input('min_price');
+        $maxPrice = $request->input('max_price');
+        $poiKey = $request->input('poi_key');
+        $poiCode = $request->input('poi_code');
+        $longitude = $request->input('longitude');
+        $latitude = $request->input('latitude');
+        $keyWords = $request->input('keywords');
+        if (empty($page) || empty($limit)) {
+            throw new LogicException('页码和每页条数必须');
+        }
+        try {
+            $res = $this->service->getPageList(
+                $page,
+                $limit,
+                $cityName,
+                $inDate,
+                $outDate,
+                $sortKey,
+                $star,
+                $minPrice,
+                $maxPrice,
+                $poiKey,
+                $poiCode,
+                $longitude,
+                $latitude,
+                $keyWords
+            );
+        } catch (Exception $e) {
+            throw new LogicException($e->getMessage());
+        }
+        return apiSuccess($res);
+    }
 }
--- app/Services/ShowApi/OrderHotelService.php
+++ app/Services/ShowApi/OrderHotelService.php
@@ -2,7 +2,71 @@
 
 namespace App\Services\ShowApi;
 
+use Exception;
+use Wanwei\Api\HotelOrder;
+
 class OrderHotelService
 {
-
+    /**
+     * Description:
+     *
+     * @param  string  $page
+     * @param  string  $limit
+     * @param  string  $cityName
+     * @param  string  $inDate
+     * @param  string  $outDate
+     * @param  string  $sortKey
+     * @param  string  $star
+     * @param  string  $minPrice
+     * @param  string  $maxPrice
+     * @param  string  $poiKey
+     * @param  string  $poiCode
+     * @param  string  $longitude
+     * @param  string  $latitude
+     * @param  string  $keyWords
+     *
+     * @return mixed
+     * @throws \Exception
+     * @author lidong<947714443@qq.com>
+     * @date   2021/6/30 0030
+     */
+    public function getPageList(
+        $page,
+        $limit,
+        $cityName,
+        $inDate,
+        $outDate,
+        $sortKey,
+        $star,
+        $minPrice,
+        $maxPrice,
+        $poiKey,
+        $poiCode,
+        $longitude,
+        $latitude,
+        $keyWords
+    ) {
+        try {
+            $HotelOrder = new HotelOrder();
+            $res = $HotelOrder->hotelSearch(
+                $page,
+                $limit,
+                $cityName,
+                $inDate,
+                $outDate,
+                $sortKey,
+                $star,
+                $minPrice,
+                $maxPrice,
+                $poiKey,
+                $poiCode,
+                $longitude,
+                $latitude,
+                $keyWords
+            );
+        } catch (Exception $e) {
+            throw $e;
+        }
+        return $res;
+    }
 }
