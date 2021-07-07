f93aa87922c75876fa93efb2c53b67152cbea33d
ning <ning@qq.com>
2021-07-07 16:45:49 +0800

    优化修改手机接口

--- routes/api/user.php
+++ routes/api/user.php
@@ -30,7 +30,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     //修改姓名
     Route::post('change-real-name', 'User\UserController@changeRealName');
     //修改用户手机号
-    Route::post('updateUserPhone', 'User\UserController@updateUserPhone');
+    Route::post('update-user-phone', 'User\UserController@updateUserPhone');
 });
 //新增消费者统计和新增商家统计
 Route::any("addConsumer", "User\CountUserController@addConsumer");
