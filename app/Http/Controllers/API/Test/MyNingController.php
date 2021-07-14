44ad4c0fa7b2c18c0696b2a6129601a5b9676e5b
ning <ning@qq.com>
2021-07-14 15:02:03 +0800

    调试推荐补贴积分记录

--- app/Http/Controllers/API/Test/MyNingController.php
+++ app/Http/Controllers/API/Test/MyNingController.php
@@ -33,6 +33,7 @@ use App\Services\AssetsService;
 use App\Services\TransferService;
 use App\Services\AssetConversionService;
 use App\Models\TtshopUser;
+
 class MyNingController extends Controller
 {
     //test测试
@@ -183,8 +184,8 @@ class MyNingController extends Controller
         $userInfo = User::where('id', $uid)->first();
         $phoneUser = User::where('phone', $phone)->first();
 
-        if ($phoneUser){
-            return "该手机号已被uid=".$phoneUser->id." 的用户使用，请更换其他手机号";
+        if ($phoneUser) {
+            return "该手机号已被uid=" . $phoneUser->id . " 的用户使用，请更换其他手机号";
         }
         if ($userInfo) {
             DB::beginTransaction();
@@ -192,7 +193,7 @@ class MyNingController extends Controller
                 $userDataLogModel = new UserUpdatePhoneLogSd();
                 $userDataLogModel->user_id = $uid;
                 $userDataLogModel->time = time();
-                $userDataLogModel->edit_to_phone = $userInfo->phone.'=>'.$phone;
+                $userDataLogModel->edit_to_phone = $userInfo->phone . '=>' . $phone;
                 $userDataLogModel->save();
 
                 $userInfo->phone = $phone;
@@ -216,7 +217,7 @@ class MyNingController extends Controller
     public function getUserAssetInfo(Request $request)
     {
         $uid = $request->input('uid');
-        $user = User::where('id',$uid)->first();
+        $user = User::where('id', $uid)->first();
 //        dd($user);
         $iets_asset = AssetsType::where('assets_name', 'iets')->first();
         $usdt_asset = AssetsType::where('assets_name', AssetsType::DEFAULT_ASSETS_NAME)->first();
@@ -234,41 +235,45 @@ class MyNingController extends Controller
     }
 
     //解封用户资产账号
-    public function xfUserAssetFH(Request $request){
+    public function xfUserAssetFH(Request $request)
+    {
         $uid = $request->input('uid');
         $amount = $request->input('amount');
         $assType = $request->input('assType');
-        $assData = Assets::where('uid',$uid)->where('assets_name',$assType)->first();
+        $assData = Assets::where('uid', $uid)->where('assets_name', $assType)->first();
         $assData->amount = $amount;
-        if($assData->save()){
+        if ($assData->save()) {
             return "账号解封成功";
-        }else{
+        } else {
             return "账号解封失败";
         }
 
     }
 
     //初始化导入记录
-    public function initDrOrderLog(Request $request){
+    public function initDrOrderLog(Request $request)
+    {
         $type = $request->input('type');// mch_order
         $order_id = $request->input('order_id');
-        if($order_id==''){
+        if ($order_id == '') {
             $order_id = 0;
         }
         echo "初始化导入记录";
-        $logData = LkshopOrderLog::where('type',$type)->first();
+        $logData = LkshopOrderLog::where('type', $type)->first();
         $logData->order_id = $order_id;
-        if($logData->save()){
-            echo "初始化成功，order_id=".$order_id;
-        }else{
+        if ($logData->save()) {
+            echo "初始化成功，order_id=" . $order_id;
+        } else {
             echo "初始化失败";
         }
 
     }
+
     //查看导入时间
-    public function getAddOrderTime(){
-        $data[] = LkshopOrderLog::where('type','mch_order')->value('order_id');
-        $data[] = LkshopOrderLog::where('type','1688_order')->value('order_id');
+    public function getAddOrderTime()
+    {
+        $data[] = LkshopOrderLog::where('type', 'mch_order')->value('order_id');
+        $data[] = LkshopOrderLog::where('type', '1688_order')->value('order_id');
 
         dd($data);
     }
@@ -298,68 +303,69 @@ class MyNingController extends Controller
 //    }
 
 //初始化修改用户手机号记录
-    public function clearUserPhoneUpdateLog(){
+    public function clearUserPhoneUpdateLog()
+    {
         $re1 = DB::table('user_update_phone_log')->truncate();
         dd($re1);
     }
 
 //扣除用户商城积分
-    public function kcUserShopJf(Request $request){
+    public function kcUserShopJf(Request $request)
+    {
         $userId = $request->input('uid');//用户uid
         $role = $request->input('role');
         $num = $request->input('num');
 
 //        var_dump($userId,$role,$num);
         echo '扣除用户积分接口<br/><br/>参数：uid用户的uid<br/>role=1表示删除消费者积分，role=2表示删除商家积分<br/>num=要删除的积分<br/><br/>操作结果：<br/><br/>';
-        $userInfo = Users::where('id',$userId)->first();
-        if ($userInfo!=''){
-            echo "当前用户的消费积分：".$userInfo->integral."<br/>";
-            echo "当前用户的商家积分：".$userInfo->business_integral."<br/>";
+        $userInfo = Users::where('id', $userId)->first();
+        if ($userInfo != '') {
+            echo "当前用户的消费积分：" . $userInfo->integral . "<br/>";
+            echo "当前用户的商家积分：" . $userInfo->business_integral . "<br/>";
             if ($role == 1) {
-                $userInfo->integral = $userInfo->integral-$num;
-                if($userInfo->save()){
-                    echo "扣除成功<br/>扣除uid=".$userId." 的用户消费者积分，".$num."积分<br/>";
+                $userInfo->integral = $userInfo->integral - $num;
+                if ($userInfo->save()) {
+                    echo "扣除成功<br/>扣除uid=" . $userId . " 的用户消费者积分，" . $num . "积分<br/>";
                 }
             } elseif ($role == 2) {
-                $userInfo->business_integral = $userInfo->business_integral-$num;
-                if($userInfo->save()){
-                    echo "扣除成功<br/>扣除uid=".$userId." 的用户商家积分，".$num."积分<br/>";
+                $userInfo->business_integral = $userInfo->business_integral - $num;
+                if ($userInfo->save()) {
+                    echo "扣除成功<br/>扣除uid=" . $userId . " 的用户商家积分，" . $num . "积分<br/>";
                 }
-            }else{
+            } else {
                 echo '扣除积分失败<br/>';
             }
-        }else{
+        } else {
             echo '该uid用户不存在<br/>';
         }
 
     }
 
     //清空商城卡单处理
-    public function setShopKdOrderId(Request $request){
+    public function setShopKdOrderId(Request $request)
+    {
         $orderId = $request->input('orderId');
-        if($orderId){
-            echo '修改OrderId'.$orderId.'的记录<br/>';
-            $orderInfo = Order::where('id',$orderId)->first();
+        if ($orderId) {
+            echo '修改OrderId' . $orderId . '的记录<br/>';
+            $orderInfo = Order::where('id', $orderId)->first();
             $orderInfo->line_up = 0;
-            if($orderInfo->save()){
+            if ($orderInfo->save()) {
                 echo '修改成功';
-            }else{
+            } else {
                 echo '修改失败';
             }
 
-        }else{
+        } else {
             echo 'orderId不能为空';
         }
-
-
-
 
 
     }
 
     //批量修改商家信息表审核状态
-    public function plUpdateBussStutas(){
-        $data = array('is_status'=>2);
+    public function plUpdateBussStutas()
+    {
+        $data = array('is_status' => 2);
         $re = DB::table('business_data')->update($data);
 
         var_dump($re);
@@ -367,28 +373,33 @@ class MyNingController extends Controller
 
 
     //同商城用户的uid
-    public function updateLkShopUserId(Request $request){
+    public function updateLkShopUserId(Request $request)
+    {
         $start = $request->input('start');
         $end = $request->input('end');
         ini_set("max_execution_time", 0);
         set_time_limit(0);
-        $shopUserData = TtshopUser::get(['id','binding']);
-//dd($shopUserData->toArray());
-//dd(count($shopUserData->toArray()));
-        $i =0;
-        foreach($shopUserData->toArray() as $v){
-            $userInfo = Users::where('phone',$v['binding'])->first();
-            if ($userInfo!=''){
-                $userInfo->shop_uid = $v['id'];
-                $userInfo->save();
-                $i++;
+//        $shopUserData = TtshopUser::get(['id','binding']);
+        dump($start,$end);
+        if ($start=='' || $end=='') {
+            dd('没有传limit范围');
+        } else {
+            $shopUserData = TtshopUser::offset($start)->limit($end)->get(['id', 'binding']);
+//            dd($shopUserData->toArray());
+//            dd(count($shopUserData->toArray()));
+            $i = 0;
+            foreach ($shopUserData->toArray() as $v) {
+                $userInfo = Users::where('phone', $v['binding'])->first();
+                if ($userInfo != '') {
+                    $userInfo->shop_uid = $v['id'];
+                    $userInfo->save();
+                    $i++;
+                }
+
             }
 
+            var_dump($i);
         }
-
-var_dump($i);
-
-
 
 
 
