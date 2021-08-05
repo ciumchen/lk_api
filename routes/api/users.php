c197c97197fe7862077b9bfa969501692793d823
ning <ning@qq.com>
2021-08-05 16:25:44 +0800

    调试购买会员接口

--- routes/api/users.php
+++ routes/api/users.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-
+//购买会员
+    Route::post('PurchaseLkMember', 'User\UsersController@PurchaseLkMember');
 });
 
-//购买会员
-Route::post('PurchaseLkMember', 'User\UsersController@PurchaseLkMember');
 //支付回调 getLkMemberPayHd
 Route::post('getLkMemberPayHd', 'User\UsersController@getLkMemberPayHd');
 
