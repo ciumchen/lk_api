a1039ed2f1c0ea209672aa4d7e24dd7ee563e81f
laydown <947714443@qq.com>
2021-08-06 14:46:18 +0800

    auth login test

---
+++ app/Http/Controllers/API/ThirdAuth/AlipayAuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\ThirdAuth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AlipayAuthController extends Controller
+{
+    //
+}
---
+++ routes/api/thirdauth.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+///////////////
+// 需要登录验证//
+///////////////
+Route::middleware(['auth:sanctum'])->group(
+    function () {
+        Route::any('alipay-auth', 'ThirdAuth\AlipayAuthController@callback');
+    }
+);
+/////////////////
+// 不需要登录验证//
+////////////////
+Route::any('alipay-notify', 'ThirdAuth\AlipayNotifyController@authNotify');
+
+
