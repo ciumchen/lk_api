d3b783d5e4bbe772f84e817dd4cf6f7ea8aa4312
laydown <947714443@qq.com>
2021-08-09 14:47:47 +0800

    auth test

--- routes/api/thirdauth.php
+++ routes/api/thirdauth.php
@@ -13,6 +13,7 @@ Route::middleware(['auth:sanctum'])->group(
         Route::any('alipay-auth-h5', 'ThirdAuth\AlipayAuthController@getAuthUrlH5');
         // 用戶綁定
         Route::post('alipay-bind', 'ThirdAuth\AlipayAuthController@userBinding');
+        Route::post('alipay-bind-check', 'ThirdAuth\AlipayAuthController@userBindingCheck');
     }
 );
 /** ************************************************************************** **/
