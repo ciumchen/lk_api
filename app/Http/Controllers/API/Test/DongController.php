618eb1893f1a608f8a905eec44cd13ce9f73ce32
laydown <947714443@qq.com>
2021-08-13 11:59:39 +0800

    config test

--- app/Http/Controllers/API/Test/DongController.php
+++ app/Http/Controllers/API/Test/DongController.php
@@ -21,7 +21,8 @@ class DongController extends Controller
      */
     public function __construct()
     {
-        dump(iconv('UTF-8', 'UTF-8//IGNORE', '这特么是什么'));
+        dump(iconv('UTF-8', 'GBK//IGNORE', '这特么是什么GBK'));
+        dump(iconv('UTF-8', 'UTF-8//IGNORE', '这特么是什么UTF-8'));
         die('测试接口');
     }
     
