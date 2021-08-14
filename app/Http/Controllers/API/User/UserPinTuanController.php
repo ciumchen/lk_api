d3f94a28696cee7b9090eb1c77de53b4cb2ec8c0
ning <ning@qq.com>
2021-08-14 11:39:32 +0800

    购买来拼金接口

--- app/Http/Controllers/API/User/UserPinTuanController.php
+++ app/Http/Controllers/API/User/UserPinTuanController.php
@@ -15,25 +15,27 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\API\Payment\YuntongPayController;
 use App\Http\Requests\UserPinTuan as ReUserPinTuan;
+
 class UserPinTuanController extends Controller
 {
 
     //购买来拼金
-    public function UserBuyLpj(Request $request){
+    public function UserBuyLpj(Request $request)
+    {
         $user = $request->user();
         $ip = $request->input('ip');
         $money = $request->input('money');
-        $ip!=''?:$ip='183.14.29.143';
+        $ip != '' ?: $ip = '183.14.29.143';
 
         $order_no = createOrderNo();
         //创建充值记录
         $data = array(
-            'uid'=>$user->id,
-            'operate_type'=>'recharge',
-            'money'=>$money,
-            'money_before_change'=>$user->balance_tuan,
-            'order_no'=>$order_no,
-            );
+            'uid' => $user->id,
+            'operate_type' => 'recharge',
+            'money' => $money,
+            'money_before_change' => $user->balance_tuan,
+            'order_no' => $order_no,
+        );
         $lpjLog = UserPinTuan::create($data);
 
 //        dd($lpjLog);
@@ -42,11 +44,11 @@ class UserPinTuanController extends Controller
         $payModel = new YuntongPayController();
         $data = [
             'goodsTitle' => '充值来拼金',
-            'goodsDesc'  => '充值来拼金',//商品描述
-            'need_fee'   => $money,//消费金额
-            'order_no'   => $order_no,//订单号
+            'goodsDesc' => '充值来拼金',//商品描述
+            'need_fee' => $money,//消费金额
+            'order_no' => $order_no,//订单号
             'order_from' => 'alipay',//支付渠道 固定值：alipay|wx|unionpay
-            'ip'         => $ip,//ip
+            'ip' => $ip,//ip
 //            'return_url' => "http://ning.catspawvideo.com/api/getLkMemberPayHd",
             'return_url' => "",
         ];
@@ -55,22 +57,33 @@ class UserPinTuanController extends Controller
     }
 
     //购买会员支付回调
-    public function getUserBuyLpjHd(Request $request){
+    public function getUserBuyLpjHd(Request $request)
+    {
         $Pay = new YuntongPay();
         $json = $request->getContent();
         DB::beginTransaction();
         try {
             $data = json_decode($json, true);
             $res = $Pay->Notify($data);
-            Log::info("=======打印充值来拼金支付回调数据====1======",$data);
+//            Log::info("=======打印充值来拼金支付回调数据====1======", $data);
             if (!empty($res)) {
-                Log::info("=======打印充值来拼金支付回调数据====2======",$data);
-
-                //修改用户来拼金
-//            $user->balance_tuan = $user->balance_tuan+$money;
-//            $user->save();
-
-
+//                Log::info("=======打印充值来拼金支付回调数据====2======", $data);
+                if ($data['type'] == "payment.success") {
+                    $lpjLog = UserPinTuan::where('order_no', $data['order_id'])->first();
+                    if ($lpjLog != null) {
+                        //修改充值记录状态
+                        $lpjLog->status = 2;
+                        $lpjLog->save();
+                        //修改用户来拼金
+                        $userInfo = Users::where('id', $lpjLog->uid)->first();
+                        $userInfo->balance_tuan = $userInfo->balance_tuan + $data['amount'];
+                        $userInfo->save();
+                    } else {
+                        Log::info("=======打印充值来拼金支付回调数据=====保存数据错误1=====", $data);
+                    }
+                } else {
+                    Log::info("=======打印充值来拼金支付回调数据=====保存数据错误2=====", $data);
+                }
 
             } else {
                 Log::info("=======打印充值来拼金支付回调数据=====解析为空=====");
@@ -80,7 +93,7 @@ class UserPinTuanController extends Controller
             $Pay->Notify_success();
         } catch (Exception $e) {
             DB::rollBack();
-            Log::debug('YuntongNotify-打印充值来拼金支付回调数据-验证不通过-getUserBuyLpjHd-'.$e->getMessage(), [$json.'---------'.json_encode($e)]);
+            Log::debug('YuntongNotify-打印充值来拼金支付回调数据-验证不通过-getUserBuyLpjHd-' . $e->getMessage(), [$json . '---------' . json_encode($e)]);
             $Pay->Notify_failed();
         }
 
