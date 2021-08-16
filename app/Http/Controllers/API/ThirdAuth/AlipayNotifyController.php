2469cbb462f5122d45a8b960506948f2d7bcd12b
laydown <947714443@qq.com>
2021-08-16 11:42:34 +0800

    messageNotify

--- app/Http/Controllers/API/ThirdAuth/AlipayNotifyController.php
+++ app/Http/Controllers/API/ThirdAuth/AlipayNotifyController.php
@@ -20,4 +20,22 @@ class AlipayNotifyController extends Controller
         Log::debug('Alipay-$get', [json_encode($get)]);
         Log::debug('Alipay-$json', [json_encode($json)]);
     }
+    
+    /**
+     * Description:消息通知地址
+     *
+     * @author lidong<947714443@qq.com>
+     * @date   2021/8/16 0016
+     */
+    public function messageNotify(Request $request)
+    {
+        $data_all = $request->all();
+        Log::debug('Alipay-messageNotify-$data_all', [json_encode($data_all)]);
+        $post = $_POST;
+        $get = $_GET;
+        $json = file_get_contents("php://input");
+        Log::debug('Alipay-messageNotify-$post', [json_encode($post)]);
+        Log::debug('Alipay-messageNotify-$get', [json_encode($get)]);
+        Log::debug('Alipay-messageNotify-$json', [json_encode($json)]);
+    }
 }
