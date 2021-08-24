1d90bac088cf06c904abbc73daa69f99c0138852
ning <ning@qq.com>
2021-08-24 16:33:22 +0800

    添加手动修改购物卡兑换代充积分添加的脚步2

--- app/Http/Controllers/API/Test/MyNingController.php
+++ app/Http/Controllers/API/Test/MyNingController.php
@@ -882,21 +882,18 @@ a{font-size: 20px;text-decoration:none;font-weight: 400;line-height: 1.42;positi
 
     //手动处理修改购物卡兑换代充没有添加积分的记录
     public function sdUpdateDchfjf(){
+        set_time_limit(0);
+        ini_set('max_execution_time', '0');
         $data = Order::where(['name'=>'代充','status'=>1,'pay_status'=>'succeeded'])->get();
         $arr = $data->toArray();
-        dd(count($arr));
-        foreach ($arr as $k=>$v){
-            (new OrderService())->addOrderIntegral($v['id']);
+        echo count($arr);
+        if (count($arr)!=0){
+            foreach ($arr as $k=>$v){
+                (new OrderService())->addOrderIntegral($v['id']);
+            }
         }
-        $oid = $request->input('oid');
-        $orderData = Order::find($oid);
-        if ($orderData!=null){
-            //通过审核添加积分，更新order 表审核状态
-            (new OrderService())->addOrderIntegral($oid);
-            echo "修改成功";
-        }else{
-            echo "oid订单不纯在";
-        }
+
+        echo "修改成功";
 
 
     }
