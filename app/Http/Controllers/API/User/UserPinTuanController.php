a7e93e9f6fdf3faad1eada0ab15601b81564d3d5
ning <ning@qq.com>
2021-09-02 11:10:43 +0800

    购物卡兑æ添加¢代充支付回调方法

--- app/Http/Controllers/API/User/UserPinTuanController.php
+++ app/Http/Controllers/API/User/UserPinTuanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\User;
 
 use App\Exceptions\LogicException;
+use App\Http\Controllers\API\Message\UserMsgController;
 use App\Http\Controllers\API\Order\RechargeController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GwkDhDefaultAuthRequest;
@@ -15,6 +16,7 @@ use App\Models\GatherShoppingCard;
 use App\Models\GwkZfOperationLog;
 use App\Models\Order;
 use App\Models\OrderMobileRecharge;
+use App\Models\RechargeLogs;
 use App\Models\Setting;
 use App\Models\TradeOrder;
 use App\Models\User;
@@ -541,6 +543,50 @@ class UserPinTuanController extends Controller
 
     }
 
+
+    //购物卡兑换代充支付回调
+    public function gwkDhHfHd(Request $request){
+        $data = $request->all();
+//        Log::debug('MobileNotify', [json_encode($data)]);
+        /*
+        {
+        "user_id": "A5626842",
+        "sign": "C0F9E3501C0DB8EBA781993D8268B073FBF9EE79",
+        "recharge_state": "1",
+        "outer_tid": "PY_20210605210408281427",
+        "tid": "S2106052397812",
+        "timestamp": "2021-06-05 21:05:12"
+        }
+        */
+        try {
+            $MobileRechargeService = new MobileRechargeService();
+            $MobileRechargeService->notify($data);
+            if ($data[ 'recharge_state' ] == 1) {
+                $recharge = new RechargeLogs();
+                $recharge = $recharge->where('reorder_id', '=', $data[ 'tid' ])
+                    ->first();
+                if (!empty($recharge)) {
+                    $recharge->created_at = date("Y-m-d H:i:s");
+                    $recharge->updated_at = date("Y-m-d H:i:s");
+                    $recharge->save();
+                } else {
+                    $recharge = new RechargeLogs();
+                    $recharge->reorder_id = $data[ 'tid' ];
+                    $recharge->order_no = $data[ 'outer_tid' ];
+                    $recharge->type = 'ZL';
+                    $recharge->status = 1;
+                    $recharge->created_at = date("Y-m-d H:i:s");
+                    $recharge->updated_at = date("Y-m-d H:i:s");
+                    $recharge->save();
+                }
+            }
+            (new UserMsgController())->setMsg($data[ 'outer_tid' ], 1);
+        } catch (\Exception $e) {
+            Log::debug('MobileNotify', [json_encode($data)]);
+            die('failed');
+        }
+        die('success');
+    }
     //购物卡兑换美团生成订单
     public function ShoppingCardDhMt(Request $request)
     {
