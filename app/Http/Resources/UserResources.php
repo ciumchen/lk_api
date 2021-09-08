2e40c3ff3ab4a4cca165e2dad15af9ec9b977777
laydown <947714443@qq.com>
2021-09-08 15:13:21 +0800

    add user_level_id

--- app/Http/Resources/UserResources.php
+++ app/Http/Resources/UserResources.php
@@ -6,6 +6,8 @@ use App\Models\AssetsType;
 use App\Models\Order;
 use App\Models\User;
 use App\Models\TradeOrder;
+use App\Models\UserLevel;
+use App\Models\UserLevelRelation;
 use App\Services\AssetsService;
 use App\Services\QrcodeService;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -13,10 +15,11 @@ use Illuminate\Support\Facades\Log;
 
 class UserResources extends JsonResource
 {
+    
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      *
      * @return array
      */
@@ -35,6 +38,8 @@ class UserResources extends JsonResource
         $encourage = AssetsService::getBalanceData($user, $encourageType);
         $share_url = env('HTTP_URL').'/register?invite_code='.$this->code_invite;
         $qrcode_url = (new QrcodeService())->userShareQrcode($this->id, $share_url);
+        $userLevelRelation = UserLevelRelation::whereUserId($user->id)->first();
+        $LevelName = UserLevel::getLevelText($userLevelRelation->level_id ?? 0);
         return [
             'id'                       => $this->id,
             'phone'                    => $this->phone,
@@ -47,7 +52,7 @@ class UserResources extends JsonResource
             'business_integral'        => format_decimal($this->business_integral),
             'integral'                 => rtrim_zero(format_decimal($this->integral)),
             'return_integral'          => rtrim_zero(format_decimal(bcadd($this->return_integral,
-                                                                          $this->return_business_integral, 8))),
+                $this->return_business_integral, 8))),
             'return_business_integral' => rtrim_zero(format_decimal($this->return_business_integral)),
             'my_spent'                 => rtrim_zero(format_decimal($mySpent)),
             'amount'                   => rtrim_zero($balance->amount ?? 0),
@@ -66,6 +71,8 @@ class UserResources extends JsonResource
             'real_name'                => $this->real_name,
             'qrcode'                   => $qrcode_url,
             'member_status'            => $this->member_status,
+            'level_id'                 => $userLevelRelation->level_id ?? 0,
+            'level_name'               => $LevelName,
         ];
     }
 }
